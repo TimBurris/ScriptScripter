ee8d697b4159b19cd71a9bb5abda4fdb56a6701c
Tim Burris <tim.burris@chalklogic.com>
2020-03-24 16:21:08 -0400

    fixed a nesting/refactor quirk

--- ScriptScripter.Command/IncomingOptions.cs
+++ ScriptScripter.Command/IncomingOptions.cs
@@ -2,30 +2,27 @@
 using System;
 namespace ScriptScripter.Command
 {
-    partial class Program
+    public class IncomingOptions
     {
-        public class IncomingOptions
-        {
-            [Option('f', "scriptfilepath", Required = true, HelpText = "The full path to the script file that is to be applied")]
-            public string ScriptFilePath { get; set; }
+        [Option('f', "scriptfilepath", Required = true, HelpText = "The full path to the script file that is to be applied")]
+        public string ScriptFilePath { get; set; }
 
-            [Option('s', "server", Required = true, HelpText = "The server name (with instance) to connect to")]
-            public String Server { get; set; }
+        [Option('s', "server", Required = true, HelpText = "The server name (with instance) to connect to")]
+        public String Server { get; set; }
 
-            [Option('d', "databasename", Required = true, HelpText = "The database to apply the scripts to")]
-            public String DatabaseName { get; set; }
+        [Option('d', "databasename", Required = true, HelpText = "The database to apply the scripts to")]
+        public String DatabaseName { get; set; }
 
-            [Option('t', "trustedconnection", Required = false, HelpText = "Optional, when specifed, connect to the database using Windows Authentication")]
-            public bool UseTrustedConnection { get; set; }
+        [Option('t', "trustedconnection", Required = false, HelpText = "Optional, when specifed, connect to the database using Windows Authentication")]
+        public bool UseTrustedConnection { get; set; }
 
-            [Option('u', "user", Required = false, HelpText = "Optional, user for the database connection use when not using Trusted Connection")]
-            public String Username { get; set; }
+        [Option('u', "user", Required = false, HelpText = "Optional, user for the database connection use when not using Trusted Connection")]
+        public String Username { get; set; }
 
-            [Option('p', "password", Required = false, HelpText = "Optional, password for the database connection use when not using Trusted Connection")]
-            public String Password { get; set; }
+        [Option('p', "password", Required = false, HelpText = "Optional, password for the database connection use when not using Trusted Connection")]
+        public String Password { get; set; }
 
-            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
-            public bool Verbose { get; set; }
-        }
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
     }
 }
