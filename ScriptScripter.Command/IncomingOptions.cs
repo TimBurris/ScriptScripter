7928cc8ca09bdabf78ab4a1e19869a96080e5390
Tim Burris <tim.burris@chalklogic.com>
2020-03-24 09:48:10 -0400

    correct typos

--- ScriptScripter.Command/IncomingOptions.cs
+++ ScriptScripter.Command/IncomingOptions.cs
@@ -15,15 +15,14 @@ namespace ScriptScripter.Command
             [Option('d', "databasename", Required = true, HelpText = "The database to apply the scripts to")]
             public String DatabaseName { get; set; }
 
-            [Option('t', "trustedconnection", Required = false, HelpText = "When specifed, connect to the database using Windows Authentication")]
+            [Option('t', "trustedconnection", Required = false, HelpText = "Optional, when specifed, connect to the database using Windows Authentication")]
             public bool UseTrustedConnection { get; set; }
 
-            [Option('p', "password", Required = false, HelpText = "Optional, passord for the database connection use when not using Trusted Connection")]
-            public String Password { get; set; }
-
-            [Option('u', "user", Required = false, HelpText = "Optional, passord for the database connection use when not using Trusted Connection")]
+            [Option('u', "user", Required = false, HelpText = "Optional, user for the database connection use when not using Trusted Connection")]
             public String Username { get; set; }
 
+            [Option('p', "password", Required = false, HelpText = "Optional, password for the database connection use when not using Trusted Connection")]
+            public String Password { get; set; }
 
             [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
             public bool Verbose { get; set; }
